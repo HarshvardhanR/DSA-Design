c006026a7f613599fd18a79f7f4e81fa365fc00b
HarshvardhanR <rharsh989878@gmail.com>
2026-02-16 16:22:03 -0500

    added solution for timeMap

--- TimeBasedKeyValueStore/TimeMap.java
+++ TimeBasedKeyValueStore/TimeMap.java
@@ -1,17 +1,54 @@
 package TimeBasedKeyValueStore;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class TimeMap {
+    Map<String, Map<Integer, String>> map;
 
     public TimeMap(){
-
+        map = new HashMap<>();
     }
 
     public void set(String key, String value, int timestamp){
-
+        if(!map.containsKey(key)){
+            map.put(key, new TreeMap<>());
+        }
+        Map<Integer, String> tempMap = map.get(key);
+        tempMap.put(timestamp, value);
     }
 
     public String get(String key, int timestamp){
+        if(!map.containsKey(key)) return "";
+        Map<Integer, String> tempMap = map.get(key);
+        List<Integer> temp = new ArrayList<>(tempMap.keySet());
+        return binarySearch(temp, tempMap, timestamp);
+    }
 
+    public String binarySearch(List<Integer> temp, Map<Integer, String> tempMap, int timestamp){
+        int low= 0;
+        int high = temp.size() - 1;
+        int result = -1;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            int tempTime = temp.get(mid);
+            if(tempTime==timestamp){
+                result = tempTime;
+                break;
+            }
+            else if(tempTime > timestamp){
+                high = mid - 1;
+            }
+            else{
+                result = tempTime;
+                low = mid + 1;
+            }
+        }
+        if(result==-1) return "";
+        return tempMap.get(result);
     }
 
     public static void main(String[] args) {
@@ -39,11 +76,3 @@ public class TimeMap {
     }
 }
 
-class Pair{
-    String key;
-    String value;
-    public Pair(String key, String value){
-        this.key = key;
-        this.value = value;
-    }
-}
