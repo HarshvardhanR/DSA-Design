9925d1c847e9e57a3267f688703cce9cf8922352
HarshvardhanR <rharsh989878@gmail.com>
2026-02-19 19:19:07 -0500

    added a problem

---
+++ InMemoryFileSystem/FileSystem.java
@@ -0,0 +1,22 @@
+package InMemoryFileSystem;
+
+import java.util.List;
+
+class FileSystem {
+
+    public void mkdir(String path){
+
+    }
+
+    public void addContentToFile(String filePath, String content){
+
+    }
+
+    public String readContentFromFile(String filePath){
+
+    }
+
+    public List<String> ls(String path){
+        
+    }
+}
